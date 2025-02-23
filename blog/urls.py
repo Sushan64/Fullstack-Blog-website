20a8c665dc796525a573326ae7863b85bae81eaf
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2025-02-23 10:24:52 +0000

    Added Pagination! More better

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path("", views.index, name="home"),
   path('post/<int:pk>/', views.post, name="post"),
+  path('category/<str:category_value>/', views.category, name="category"),
 ]
