26e162b4f2e5b636a88c5ab47954552fa93fe141
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2025-03-20 15:45:17 +0000

    Changed a lot for render

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4ju2n@$f9d0c=h)_g0lbb%k9&@rf(xa$d$g$&5ri$uf)*gev^4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".replit.dev", ".replit.app", ".vercel.app", "*.sisko.repl.co", "5538b446-d014-427c-be5e-2fbacbdb7787-00-1krrg3vbyj3dg.sisko.repl.co"]
+ALLOWED_HOSTS = [".replit.dev", ".replit.app", ".vercel.app", ".sisko.repl.co"]
 CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app"]
 
 # Application definition
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -100,7 +100,7 @@ DATABASES = {
         "PORT": "46318",
     }
 }
-'''
+
 
 
 # Password validation
